64166cbc619d50f754324f372bf592dcf243aa8b
Parteek <parteekkumar2021@gmail.com>
2024-07-03 12:49:11 +0530

    Add All Node.js file

---
+++ script.js
@@ -0,0 +1,30 @@
+// let n=5;
+// for (let i=0; i<n;i++){
+//     console.log("hello,",i);
+// }
+
+// console.log("bye!");
+
+// //process in node
+
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("hello to ",args[i]);
+// }
+
+import {sum,PI} from "./math.js"
+console.log (PI);
+console.log(sum(1,2));
+
+
+//exports in files 
+
+const math=require("./math");
+console.log(math.sum(2,2));
+console.log(math.PI);
+
+//export in directories
+
+const info=require("./fruits");
+console.log(info);
+console.log(info[0].name);
